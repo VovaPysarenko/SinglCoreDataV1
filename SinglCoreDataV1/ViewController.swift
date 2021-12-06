05e71b6f688dd7db6b505a9cc19f694ee41dc14b
VovaPysarenko <guarder2009@gmail.com>
2021-12-06 14:09:14 +0200

    Initial Commit

---
+++ SinglCoreDataV1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SinglCoreDataV1
+//
+//  Created by Volodymyr Pysarenko on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
